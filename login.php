7818668d5933bfffa3813d1f0402f5015e39a110
Malith Geevinda Gunawardhana <malith0830@gmail.com>
2025-05-21 16:48:56 +0530

    create admin file

--- login.php
+++ login.php
@@ -124,10 +124,10 @@ include_once 'includes/header.php';
             <button type="submit" class="btn btn-primary w-100">Login</button>
         </form>
         
-        <div class="auth-links mt-3">
+        <!-- <div class="auth-links mt-3">
             <p>Don't have an account? <a href="register.php">Register</a></p>
             <p><a href="forgot_password.php">Forgot Password?</a></p>
-        </div>
+        </div> -->
     </div>
 </div>
 
